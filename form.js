37d0848ec0016523ba26e125b7625d0c1134b395
Lisis <laruschel@proton.me>
2023-01-28 20:54:54 +0100

    added border shadow to header and footer. DM buttons on every page

--- form.js
+++ form.js
@@ -90,3 +90,24 @@ updateAmountLeftQuestion(maxLength);
 question.addEventListener("input", () => {
   updateAmountLeftQuestion(maxLength - question.value.length);
 });
+
+// dark mode
+
+const darkmode_btn = document.querySelector('[data-js="darkmode"]');
+const darkmode_profileImg = document.querySelector(
+  '[data-js="profile__image" ]'
+);
+const menuBarProfileImg = document.querySelector(
+  '[data-js="profile-page__icon"]'
+);
+
+darkmode_btn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    menuBarProfileImg.src = "darkmodeprofile.png";
+  } else {
+    darkmode_profileImg.src = menuBarProfileImg.src =
+      "https://img.icons8.com/ios/50/null/cat-profile.png";
+  }
+});
